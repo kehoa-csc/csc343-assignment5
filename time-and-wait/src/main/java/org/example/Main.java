b7026c8f44aad61609feff9cf09689150c72492e
andrewkehoe <157737128+andrewkehoe@users.noreply.github.com>
2025-03-12 19:24:49 -0400

    turnaround times and prints

--- time-and-wait/src/main/java/org/example/Main.java
+++ time-and-wait/src/main/java/org/example/Main.java
@@ -2,12 +2,6 @@ package org.example;
 import java.util.Arrays;
 
 public class Main {
-    public static void print_array(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
 
     //calculates the wait times in an array of process burst times, assuming the order of processes as given
     public static int[] wait_times(int[] burst_times) {
@@ -25,25 +19,40 @@ public class Main {
         return wait_times;
     }
 
-    /*public static int[] turnaround_times(int[] burst_times) {
-        int time_passed = 0;
+    //calculates the turnaround times in an array of process burst times, assuming the order of processes as given
+    public static int[] turnaround_times(int[] burst_times) {
         int[] t_times = new int[burst_times.length]; //just shorthand
 
-
-    }*/
+        int[] wait_times = wait_times(burst_times);
+        for (int i = 0; i < burst_times.length; i++) {
+            t_times[i] = wait_times[i] + burst_times[i];
+        }
+        return t_times;
+    }
 
     public static void main(String[] args) {
         int[] burst_times = {2, 1, 8, 4, 5};
 
         //computing times for FCFS
         int[] wait_times_fcfs = wait_times(burst_times);
-        //int[] turnaround_times_fcfs = turnaround_times(burst_times);
-        print_array(wait_times_fcfs);
-        //print_array(turnaround_times_fcfs);
+        int[] turnaround_times_fcfs = turnaround_times(burst_times);
 
         //sort tasks for SJF
         Arrays.sort(burst_times);
         int[] wait_times_sjf = wait_times(burst_times);
-        print_array(wait_times_sjf);
+        int[] turnaround_times_sjf = turnaround_times(burst_times);
+
+        System.out.println("------------------- FCFS -------------------");
+        System.out.println(" Process ID |  Waiting Time | Turnaround Time");
+        for (int i = 0; i < burst_times.length; i++) {
+            System.out.println("\t"+i+"\t\t|\t\t"+wait_times_fcfs[i]+"\t\t|\t\t"+turnaround_times_fcfs[i]);
+        }
+
+        System.out.println("------------------- SJF -------------------");
+        System.out.println(" Process ID |  Waiting Time | Turnaround Time");
+        for (int i = 0; i < burst_times.length; i++) {
+            System.out.println("\t"+i+"\t\t|\t\t"+wait_times_sjf[i]+"\t\t|\t\t"+turnaround_times_sjf[i]);
+        }
+
     }
 }
